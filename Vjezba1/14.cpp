ec8da0d35c37e02deb455792783c624414422f5b
azracaus <148151440+azracaus@users.noreply.github.com>
2024-03-08 00:40:04 +0100

    Update 14.cpp

--- Vjezba1/14.cpp
+++ Vjezba1/14.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 using namespace std;
 
-/*Napisati program u kojem cete unijeti jedan prirodan broj 
-koji predstavlja vrijeme u min, a izracunati i ispisati koliko 
-je to vremena u danima, satima i minutama. Upotrijebite funkcije
-int sati (int &); int dani (int &);
-Npr ako korisnik unese 1520min program treba ispisati 
-da je to 1 dan 1 sat i 20 min.
-*/
-
 int sati(int&);  
 int dani(int&);
 
