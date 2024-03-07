899ddce41a4250d9db30c21cd6a4205e69b1f8e3
azracaus <148151440+azracaus@users.noreply.github.com>
2024-03-08 00:15:28 +0100

    Update 04.cpp

--- Vjezba1/04.cpp
+++ Vjezba1/04.cpp
@@ -4,12 +4,6 @@
 
 using namespace std;
 
-/*
-	Unesite broj n. Ispisati sve stepene broja 2 do 2^n koriste?i do-while petlju.
-	Input: n [int]
-	Output: 2^0 2^1 2^2 ... 2^n
-*/
-
 int main() {
 
 	int n, broj=0, rez;
